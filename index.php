6a373465c41d1a253a430b3fd98086d32179676d
Franco <franavarrok@gmail.com>
2025-11-04 07:55:44 -0300

    Validacion de usuario Login

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require_once 'php/sessionvalidate.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- php/sessionvalidate.php
+++ php/sessionvalidate.php
@@ -1,10 +1,7 @@
 <?php 
     session_start();
 
-    if(isset($_SESSION['user'])){
-        header("Location: ../index.php");
-
-    }else{
+    if(!isset($_SESSION['user'])){
         header("Location: ../login.php");
     }
 ?>
