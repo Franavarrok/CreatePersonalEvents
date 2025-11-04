def08bd03a398441c146a5997aa233e02176c13b
Franco <franavarrok@gmail.com>
2025-11-04 05:10:38 -0300

    Validate sessions for Login

---
+++ php/loginvalidate.php
@@ -0,0 +1,20 @@
+<?php 
+
+    require_once 'conexion.php';
+
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+
+    $sql = "SELECT * users WHERE '$email' AND pass = '$pass' AND STATUS = 1";
+    $result = $link -> query($sql);
+
+    if($result -> num_rows = 0){
+        session_start();
+        $_SESSION['user'] = $email;
+        header("Location: ../index.php");
+
+    }else{
+        header("Location: ../login.php");
+    }
+
+?>
