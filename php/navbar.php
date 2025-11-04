17514fc4466cb55ed5ad415416f3ce379bcccc23
Franco <franavarrok@gmail.com>
2025-11-03 22:59:15 -0300

    Cambio de rutas en la navbar

--- php/navbar.php
+++ php/navbar.php
@@ -1,9 +1,9 @@
 <div class="navbar">
-    <a href="../index.php"><h2 class="title">EVENTS</h2></a>
+    <a href="index.php"><h2 class="title">EVENTS</h2></a>
     <div class="links">
-        <a href="../create.php">CREATE</a>
-        <a href="../event.php">EVENTS</a>
-        <a href="../about.php">ABOUT</a>
+        <a href="create.php">CREATE</a>
+        <a href="event.php">EVENTS</a>
+        <a href="about.php">ABOUT</a>
     </div>
     <div class="button-container">
         <button class="buttonlogout">LOG OUT</button>
