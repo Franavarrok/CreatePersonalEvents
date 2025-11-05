1475136464cb33a6dec82de8c9522decdd4540dd
Franco <franavarrok@gmail.com>
2025-11-05 00:51:02 -0300

    Cambio de titulo en el archivo Index

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Events</title>
+    <title>Index</title>
 
     <!-- Vinculamos todos los archivos necesarios. -->
     <link rel="stylesheet" href="css/navbar.css">
