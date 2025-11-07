81b09af9183c887e896ff688b76262e22cf88843
Franco <franavarrok@gmail.com>
2025-11-07 13:49:01 -0300

    Disenos para el apartado del UPDATE

--- update.php
+++ update.php
@@ -16,16 +16,26 @@ if (isset($_GET['id'])) {
 <head>
     <meta charset="UTF-8">
     <title>Update Event</title>
+
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/update.css">
 </head>
 <body>
-    <h2>Update Event</h2>
+
+    <?php include 'php/navbar.php'; ?>
 
     <form action="php/updatevalidate.php" method="post">
-        <input type="hidden" name="id" value="<?= $event['id'] ?>">
-        <input type="text" name="title" value="<?= htmlspecialchars($event['title']) ?>" required><br>
-        <input type="text" name="description" value="<?= htmlspecialchars($event['description']) ?>" required><br>
-        <input type="date" name="date" value="<?= htmlspecialchars($event['date']) ?>" required><br>
-        <button type="submit" name="submit">Save Changes</button>
+        <div class="main-container">
+            <h2 class="title">UPDATE INFORMATION</h2>
+            <div class="input-container">
+                <input type="hidden" name="id" value="<?= $event['id'] ?>">
+                <input type="text" name="title" value="<?= htmlspecialchars($event['title']) ?>" required><br>
+                <input type="text" name="description" value="<?= htmlspecialchars($event['description']) ?>" required><br>
+                <input type="date" name="date" value="<?= htmlspecialchars($event['date']) ?>" required><br>
+                <button type="submit" name="submit" class="savebutton">Save Changes</button>
+            </div>
+        </div>
     </form>
+
 </body>
 </html>
