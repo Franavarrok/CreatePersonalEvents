38df91e257ef695a92a9af1822bb7df54ee4858d
Franco <franavarrok@gmail.com>
2025-11-07 07:57:47 -0300

    Cambio 5.0

--- php/sessionvalidate.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    //Comienzo de sesion.
-    session_start();
-
-    //Si la sesion del usuario es diferente, nos redirecciona al archivo 'login.php'.
-    if(!isset($_SESSION['user'])){
-        header("Location: login.php");
-    }
-?>
